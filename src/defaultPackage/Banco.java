96a23c261f54ac912c88a14e06947bc9d4823d4b
amir1226@gmail.com <amir1226@gmail.com>
2020-11-05 09:25:57 -0500

    Create Class Banco

---
+++ src/defaultPackage/Banco.java
@@ -0,0 +1,18 @@
+package defaultPackage;
+
+import java.util.List;
+
+/**
+ *
+ * @author Amir
+ */
+public class Banco {
+    
+    private String nombreBanco;
+    
+    private List <Usuario> usuarios;
+    
+    private List <Cuenta> cuentas;
+    
+    
+}
